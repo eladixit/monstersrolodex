5f09d696e9c9a44ee910075969b8223bafc85917
Ela Dixit <ela.mishra02@gmail.com>
2020-06-25 13:12:00 +0530

    adding files to repository

---
+++ build/precache-manifest.e182aaec8856db7b9c9013f18ccd6d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c00074017806a7b59a99dd12c750896",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dfe9631a0b26272137a8",
+    "url": "/monsters-rolodex/static/css/main.6474bd6f.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe9631a0b26272137a8",
+    "url": "/monsters-rolodex/static/js/main.7c6e9bfc.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
